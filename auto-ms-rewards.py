4163579c5e8ec4dfada5699ebd6f9451c4355038
人间流浪少年 <ksxmyqj@gmail.com>
2025-06-17 16:42:21 +0800

    add finally to ensure driver will be  closed

--- auto-ms-rewards.py
+++ auto-ms-rewards.py
@@ -26,67 +26,68 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 driver = webdriver.Edge(options=options)
 
-websearches = []
-driver.get("https://bing.com")
+try:
+    websearches = []
+    driver.get("https://bing.com")
 
-element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, f"//*[@id='trending_now_tile']/li[1]/a"))
-)
-
-for k in range(num_of_searches):
-    news_title = driver.find_element(By.XPATH, f"//*[@id='trending_now_tile']/li[{k+1}]/a").get_attribute("href")
-    websearches.append(news_title)
-
-print(websearches)
-#search box xpath
-search_xpath = "//*[@id='sb_form_q']"
-
-for i in range(num_of_searches):
-    #finding search box
-    search_form = driver.find_element(By.XPATH, search_xpath)
-    #clearing field
-    if(i!=0):
-        search_form.send_keys(Keys.CONTROL, "a")
-        search_form.send_keys(Keys.DELETE)
-    #waits for search bar to finish loading
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable(search_form)).click()
-    search_news = websearches[i]
-    search_form.send_keys(search_news)
-    search_form.send_keys(Keys.ENTER)
-    
-    #waits for page to finish loading
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located
+        EC.presence_of_element_located((By.XPATH, f"//*[@id='trending_now_tile']/li[1]/a"))
     )
 
-print(f"{num_of_searches} searches complete.")
+    for k in range(num_of_searches):
+        news_title = driver.find_element(By.XPATH, f"//*[@id='trending_now_tile']/li[{k+1}]/a").get_attribute("href")
+        websearches.append(news_title)
 
-#deleting history
-#opens history page
-driver.get("edge://history/all")
+    print(websearches)
+    #search box xpath
+    search_xpath = "//*[@id='sb_form_q']"
 
-miss=0
-for j in range(num_of_searches):
-    #selecting checkboxes
-    hist_xpath = f"//*[@id='list-item-card-checkbox-input-{j+7}']"
-    try:
-        hist_del = driver.find_element(By.XPATH, hist_xpath)
-        hist_del.click()
-    except:
-        miss+=1
-        continue
+    for i in range(num_of_searches):
+        #finding search box
+        search_form = driver.find_element(By.XPATH, search_xpath)
+        #clearing field
+        if(i!=0):
+            search_form.send_keys(Keys.CONTROL, "a")
+            search_form.send_keys(Keys.DELETE)
+        #waits for search bar to finish loading
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable(search_form)).click()
+        search_news = websearches[i]
+        search_form.send_keys(search_news)
+        search_form.send_keys(Keys.ENTER)
+        
+        #waits for page to finish loading
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located
+        )
 
-#delete button
-del_xpath = "//*[@id='actionDialogCardPrimaryButton']"
-driver.find_element(By.XPATH, del_xpath).click()
-#confirm button
-del_confirm_xpath = "//*[@id='confirmModalPrimaryButton']"
-driver.find_element(By.XPATH, del_confirm_xpath).click()
+    print(f"{num_of_searches} searches complete.")
 
-if(miss!=0):
-    print(f"Failed to clear {miss} elements in browser history.")
-time.sleep(3)
-for count in range(5):
-    print(f"Exiting in {5-count}...")
+    #deleting history
+    #opens history page
+    driver.get("edge://history/all")
 
-driver.close()
+    miss=0
+    for j in range(num_of_searches):
+        #selecting checkboxes
+        hist_xpath = f"//*[@id='list-item-card-checkbox-input-{j+7}']"
+        try:
+            hist_del = driver.find_element(By.XPATH, hist_xpath)
+            hist_del.click()
+        except:
+            miss+=1
+            continue
+
+    #delete button
+    del_xpath = "//*[@id='actionDialogCardPrimaryButton']"
+    driver.find_element(By.XPATH, del_xpath).click()
+    #confirm button
+    del_confirm_xpath = "//*[@id='confirmModalPrimaryButton']"
+    driver.find_element(By.XPATH, del_confirm_xpath).click()
+
+    if(miss!=0):
+        print(f"Failed to clear {miss} elements in browser history.")
+    time.sleep(3)
+    for count in range(5):
+        print(f"Exiting in {5-count}...")
+finally:
+    driver.close()
